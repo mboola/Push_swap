ccf78113f31f98c400950e3a623e34d9a296fff4
mboola <mpovilleti@gmail.com>
2023-12-22 17:34:01 +0100

    Deleted function not used

--- include/push_swap.h
+++ include/push_swap.h
@@ -50,7 +50,6 @@ typedef struct t_stack
 int		ft_atoi(char *str, char *err);
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 t_list	*ft_lstnew(void *content);
-t_list	*lst_separate(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstadd_front(t_list **lst, t_list *new);
